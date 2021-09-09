b962734ad0ad4943b1aa99ce305692a4280568a0
“yliwentao008 <676078451@qq.com>
2021-09-09 10:42:14 +0800

    完成settings的拆分

--- RoseTiger/RoseTiger/settings/base.py
+++ RoseTiger/RoseTiger/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -22,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3gt3q3#spnnpnyyzhg5m()621ry@((6w4@tv@q+l37t8%50h*x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
 
 ALLOWED_HOSTS = []
 
